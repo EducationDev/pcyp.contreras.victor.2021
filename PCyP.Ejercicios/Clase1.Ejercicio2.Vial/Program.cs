a220e792a891dead0c6f2fff1b18e3117a2f69d9
Víctor Hugo Contreras <vcontreras@gmail.com>
2021-03-25 20:37:01 -0300

    Agregar Nuevo Ejercicio Vial
    Solo cree el proyecto vacío para luego desarrollar el ejercicio

---
+++ PCyP.Ejercicios/Clase1.Ejercicio2.Vial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio2.Vial
+{
+    /// <summary>
+    /// Recorrer el archivo vial.csv, tomar cada linea, separar los campos y mostrarlos por pantalla. 
+    /// Instanciar un objeto por cada linea  que pueda registrar los campos necesarios y que exponga un método imprimir() donde muestre esos valores.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Clase1.Ejercicio2.Vial");
+        }
+    }
+}
