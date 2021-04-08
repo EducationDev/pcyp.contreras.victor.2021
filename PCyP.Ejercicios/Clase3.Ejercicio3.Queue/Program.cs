394329dbcf287cc6db81e55fed45873b291bdb20
Víctor Hugo Contreras <vcontreras@gmail.com>
2021-04-08 20:35:20 -0300

    Clase3 Ejercicio3 Implementar ConcurrentQueue

--- PCyP.Ejercicios/Clase3.Ejercicio3.Queue/Program.cs
+++ PCyP.Ejercicios/Clase3.Ejercicio3.Queue/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,54 @@ namespace Clase3.Ejercicio3.Queue
 {
     internal class Program
     {
-        
+        /// <summary>
+        /// Constante que representa el Total de números de ordenes.
+        /// </summary>
+        private const int NOrders = 5;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         internal static void Main(string[] args)
         {
+            // ConcurrentQueue
+            var ordersQueue = new ConcurrentQueue<string>();
+
+            var t1 = Task.Run(() =>
+            {
+                PlaceOrders(ordersQueue, "Javier", NOrders);
+            });
+
+            var t2 = Task.Run(() =>
+            {
+                PlaceOrders(ordersQueue, "Daniela", NOrders);
+            });
+
+            Task.WaitAll(t1, t2);
+
+
+            foreach (var order in ordersQueue)
+                Console.WriteLine("ORDEN: " + order);
+
+            
         }
 
-        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="orders"></param>
+        /// <param name="customerName"></param>
+        /// <param name="nOrders"></param>
+        internal static void PlaceOrders(ConcurrentQueue<string> orders, string customerName, int nOrders)
+        {
+            for (var i = 1; i <= nOrders; i++)
+            {
+                Thread.Sleep(1);
+                var orderName = $"{customerName} quiere una camiseta {i}";
+                orders.Enqueue(orderName);
+            }
+
+        }
     }
 }
